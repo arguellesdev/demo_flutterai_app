d02e1cc88ec5dfef4d890294e016ce19a6aa97ed
Anya <arguellesdev@gmail.com>
2025-01-07 09:18:27 -0700

    First commit, app creation, project definition by vgv cli

---
+++ demo_bussiness_app/android/app/src/main/kotlin/com/example/verygoodcore/demo/bussiness/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.demo_bussiness_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ demo_bussiness_app/lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:demo_bussiness_app/app/app.dart';
+import 'package:demo_bussiness_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
